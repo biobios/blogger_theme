631d3a3065b479e2542044e589dd80789631a63e
BioBIOS <biobios1024@gmail.com>
2024-07-07 23:43:31 +0900

    chore: Add script for localizing theme.xml file

---
+++ localize.sh
@@ -0,0 +1,13 @@
+# set -x # uncomment this line when debugging
+def_file_list=(`find ./localization_def -type f -printf "%f\n"`)
+
+for def_file in "${def_file_list[@]}"; do
+    readarray def_list < "./localization_def/${def_file}"
+    cp theme.xml "./localized/${def_file}.xml"
+    for def in "${def_list[@]}"; do
+        from="${def%%=*}"
+        to="${def#*=}"
+        to="${to%?}"
+        sed -i'' -e "s/i18n.$from/$to/g" "./localized/${def_file}.xml"
+    done
+done
